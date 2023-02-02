73fe191a80b12b9903600669be5360d1505eff1c
yurinomiyashita <yurinom@hawaii.edu>
2023-02-02 13:03:12 -1000

    xx

---
+++ Lab5/Ex1.js
@@ -0,0 +1,11 @@
+age =  21;
+fav_num = 3;
+day_of_birth = 31;
+month_of_birth = 12;
+console.log(age + fav_num / day_of_birth * month_of_birth);
+console.log((age + fav_num) / day_of_birth * month_of_birth);
+
+//a. 	Write a program that will output the results for the following two expressions:
+//age + fav_num / day_of_birth * month_of_birth
+
+//(age + fav_num) / day_of_birth * month_of_birth

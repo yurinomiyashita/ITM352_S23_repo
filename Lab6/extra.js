254ac727c69a244df098e87e9bf0a631221e6d7e
yurinomiyashita <yurinom@hawaii.edu>
2023-02-14 12:26:41 -1000

    xx

---
+++ Lab6/extra.js
@@ -0,0 +1,41 @@
+//extra credit swicth statement to see what month 
+let month;
+switch (new Date().getDay()) {
+    case 1:
+      month = "January";
+      break;
+    case 2:
+      month = "February";
+      break;
+    case 3:
+       month = "March";
+      break;
+    case 4:
+      month = "April";
+      break;
+    case 5:
+      month = "May";
+      break;
+    case 6:
+      month = "June";
+      break;
+    case 7:
+      month = "July";
+      break;
+   case 8:
+      month = "August";
+      break;
+    case 9:
+      month = "September";
+      break;
+    case 10:
+      month = "October";
+   case 11:
+      month = "November";
+      break;
+    case 12:
+      month = "December";
+      break;
+  }
+  
+console.log(month)

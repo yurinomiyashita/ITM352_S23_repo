df3b8c1f6d1082167b3630efcfe58fd54ea40b10
yurinomiyashita <yurinom@hawaii.edu>
2023-02-17 23:42:02 -1000

    xx

--- quiz.js
+++ quiz.js
@@ -1,7 +1,18 @@
-s = 0;
-x = 1;
-while (x <= 3) {
-    s = s + x;                 
-    x = x + 1; 
-}
-console.log(s,x)
+var classes_array = ['ITM352','BUS311','ACC101','FIN100','BUS310','MOM202']
+console.log(classes_array[2]);
+classes_array[2]='ITM353';
+console.log(classes_array[2]);
+classes_array.push('MKT300');
+console.log(classes_array);
+
+var student ={firstName:'Dan',lastName:'Kazman',major:'MIS',year:3,id:123456789,onProbation:true}
+console.log(student.major);
+student.onProbation=false;
+console.log(student.onProbation);
+student.clubMembership='ITMA';
+console.log(student);
+
+now_str = Date();
+now_obj = new Date();
+
+console.log(now_str,now_obj);

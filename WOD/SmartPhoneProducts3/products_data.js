b22870907ab3c7f7521151cef72f163df30134ff
yurinomiyashita <yurinom@hawaii.edu>
2023-02-27 22:32:48 -1000

    xx

---
+++ WOD/SmartPhoneProducts3/products_data.js
@@ -0,0 +1,35 @@
+ var product1 =  {
+     "name" : "HTC",
+     "price" : 40.00,
+     "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",   
+ }; 
+ var product2 = {
+"name" : "Apple",
+      "price" : 75.00,
+      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+ };
+ 
+var product3 = {
+     "name" : "Nokia",
+     "price" : 35.00,
+     "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+}
+var product4 = {
+     "name" : "Samsung",
+      "price": 45.00,
+      "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+}
+var product5 = {
+     
+      "name": "Blackberry",
+      "price" : 10.00,
+      "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+}
+      
+var products = [product1, product2, product3, product4, product5]; 
+
+     
+
+      
+
+console.log("Product iables loaded..."); 

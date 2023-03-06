811632a912c47fb9f874911775de44c8fa6fa1bf
yurinomiyashita <yurinom@hawaii.edu>
2023-03-05 20:06:20 -1000

    invoice 3

---
+++ WOD/invoice3/products_data.js
@@ -0,0 +1,43 @@
+ 
+var quantities= [2,0,1,4,-1,"cat"]
+
+ var product1 =  {
+     "name" : "HTC",
+     "price" : 40.00,
+     "quantity":quantities[0],
+     "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",   
+ }; 
+ var product2 = {
+      "name" : "Apple",
+      "price" : 75.00,
+      "quantity":quantities[1],
+      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+ };
+ 
+var product3 = {
+     "name" : "Nokia",
+     "price" : 35.00,
+     "quantity":quantities[2],
+     "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+}
+var product4 = {
+     "name" : "Samsung",
+      "price": 45.00, 
+      "quantity":quantities[3],
+      "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+}
+var product5 = {
+     
+      "name": "Blackberry",
+      "price" : 10.00,
+      "quantity":quantities[4],
+      "image": "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+}
+      
+var product = [product1, product2, product3, product4, product5]; 
+
+     
+
+      
+
+console.log("Product iables loaded..."); 

c245f88dce2a023266e0277027b28fac22c9d87e
yurinomiyashita <yurinom@hawaii.edu>
2023-03-07 12:22:41 -1000

    algorithm

---
+++ Lab10/Ex1.js
@@ -0,0 +1,16 @@
+//my birthday is 12/31/2001 
+var year = 2001; 
+var month = 12; 
+var day = 31; 
+
+step1 = 1; 
+step2 = parseInt(01/4); 
+step3 = step1+step2; 
+step4= 5; //number is taken from November 
+step6= step3+step4; 
+step7= step6+31; 
+step8= step7;
+step9= step8-1; 
+step10= step9%7;
+ 
+console.log(step10);   //output was 1 and it means Monday 

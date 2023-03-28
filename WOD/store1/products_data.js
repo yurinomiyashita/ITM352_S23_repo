b31b7527f1d14da500f288bfadc26addc82d821a
yurinomiyashita <yurinom@hawaii.edu>
2023-03-27 23:21:33 -1000

    x

---
+++ WOD/store1/products_data.js
@@ -0,0 +1,36 @@
+       // products array 
+var product=[
+        //product 1
+        product1 = {
+                name: "HTC",
+                price: 40.00,
+                image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg",
+        },
+        //product 2
+        product2 = {
+                name: "Apple",
+                price: 75.00,
+                image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg",
+        },
+        
+        //product 3
+        product3 = {
+                name: "Nokia",
+                price: 5.00,
+                image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg",
+        },
+        
+        //product 4
+        product4 = {
+                name: "Samsung",
+                price: 5.00,
+                image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg",
+        },
+        
+        //product 5
+        product5 = {
+                name: "Blackberry",
+                price: 10.00,
+                image: "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg",
+        },
+        ]

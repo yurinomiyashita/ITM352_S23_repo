19247aaa417b44d11e1348351eefc6c4d1d26590
yurinomiyashita <yurinom@hawaii.edu>
2023-03-30 13:18:30 -1000

    xx

--- Lab13/info_server_Ex2a.js
+++ Lab13/info_server_Ex2a.js
@@ -1,6 +1,6 @@
 var express = require('express');
-var app = express();
-app.all('*', function (request, response, next) {
+var app = express();   // express function because () to excute the code 
+app.all('*', function (request, response, next) { //put all function to all with infinite loop, this is call back function
     response.send(request.method + ' to path ' + request.path);
 });
 app.listen(8080, () => console.log(`listening on port 8080`)); // note the use of an anonymous function here to do a callback

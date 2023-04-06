6f71040d3173809d1e588048c61b5cbc74e682fd
yurinomiyashita <yurinom@hawaii.edu>
2023-04-06 11:24:47 -1000

    xx

---
+++ Lab13/Ex1.js
@@ -0,0 +1,8 @@
+let product = {name:'small gumball', price:0.34};
+
+let tax_rate = 0.045;
+
+let total = product.price + product.price * tax_rate;
+
+console.log(`A ${product.name} costs $${total}`); 
+
